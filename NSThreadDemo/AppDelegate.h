3a59ec32928cf964654b1de72a7903bba61dffe0
liyang2016 <ly@liyangdeimac.local>
2016-03-08 10:45:27 +0800

    Initial Commit

---
+++ NSThreadDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSThreadDemo
+//
+//  Created by ly on 16/3/8.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSThreadDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NSThreadDemo
+//
+//  Created by ly on 16/3/8.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
